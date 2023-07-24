010f31354830059a9a6b385323eff132982c3e99
Daniel Gross <bertogross@gmail.com>
2023-07-24 13:13:33 -0300

    2.0.0

---
+++ resources/views/layouts/body.blade.php
@@ -0,0 +1,3 @@
+<body data-bs-theme="dark" data-topbar="light" data-sidebar="dark">
+
+<!-- <body data-layout="horizontal"> -->
---
+++ storage/framework/views/2ae6b724ba6de19fc4dd3443de8c2dbd.php
@@ -0,0 +1,4 @@
+<body data-bs-theme="dark" data-topbar="light" data-sidebar="dark">
+
+<!-- <body data-layout="horizontal"> -->
+<?php /**PATH D:\www\superametas\application\development.superametas.com\public_html\resources\views/layouts/body.blade.php ENDPATH**/ ?>
